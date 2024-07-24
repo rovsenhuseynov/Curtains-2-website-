47073474723ae9d01433103cb9946ade7e4a41b0
rovshen <rovsen.dev@gmail.com>
2024-07-24 12:52:57 +0400

    коррекция dropdown

--- src/components/Header/HeaderNav/DropDownMenu/NavDropdownMenu.jsx
+++ src/components/Header/HeaderNav/DropDownMenu/NavDropdownMenu.jsx
@@ -5,19 +5,19 @@ import { NavLink } from "react-router-dom";
 const DropDownMenu = () => {
   return (
     <ul className="dropdown">
-      <li>
+      <li className="dropdown__item">
         <NavLink to="/tulle">TÜLLƏR</NavLink>
       </li>
-      <li>
+      <li className="dropdown__item">
         <NavLink to="/thick_curtains">QALIN PƏRDƏLƏR</NavLink>
       </li>
-      <li>
+      <li className="dropdown__item">
         <NavLink to="/subtle_blackout">GÜNƏŞLİK</NavLink>
       </li>
-      <li>
+      <li className="dropdown__item">
         <NavLink to="/blackout">GÜNKEÇİRMƏZ</NavLink>
       </li>
-      <li>
+      <li className="dropdown__item">
         <NavLink to="/peculiarities">BİLGİLƏR</NavLink>
       </li>
     </ul>
